77fd260ac29a450683bb2e385137813d16701c63
todd <toddgh@acquirus.com>
2025-02-04 10:49:35 +0900

    enable fix subcommand of cargo audit

--- todd/zsh/setup_rust.sh
+++ todd/zsh/setup_rust.sh
@@ -3,7 +3,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup component add rust-src
 rustup component add rust-analyzer
 cargo install cargo-edit
-cargo install cargo-audit
+cargo install cargo-audit --locked --features=fix
 cargo install cargo-llvm-cov
 cargo install cargo-nextest
 cargo install diesel_cli
