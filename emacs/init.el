1610b0872125f263d0560f06d7d7af933b7267af
Todd Ornett <toddgh@acquirus.com>
2025-02-13 16:11:58 +0900

    reconfigure auto-package-update

--- emacs/init.el
+++ emacs/init.el
@@ -46,13 +46,12 @@
 (setq use-package-compute-statistics t)
 
 (use-package auto-package-update
-  :custom
-  (auto-package-update-interval 7)
-  (auto-package-update-prompt-before-update t)
-  (auto-package-update-hide-results t)
+  :ensure t
   :config
-  (auto-package-update-maybe)
-  (auto-package-update-at-time "09:00"))
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-interval 7)
+  (setq auto-package-update-prompt-before-update t)
+  (auto-package-update-maybe))
 
 (use-package exec-path-from-shell
   :config
