be74413783af834b79204b692ae48ee13a380ef6
Todd Ornett <toddgh@acquirus.com>
2025-03-26 20:49:59 +0900

    add emacs installer used for doom emacs

---
+++ todd/zsh/install_emacs_for_doom.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+brew tap railwaycat/emacsmacport
+brew reinstall emacs-mac --with-dbus\
+   --with-modules\
+   --with-starter\
+   --with-imagemagick\
+   --with-no-title-bars\
+   --with-emacs-icons-project-EmacsIcon7\
+   --with-native-comp\
+   --with-mac-metal\
+   --with-xwidgets
+ln -sf $HOMEBREW/opt/emacs-mac/Emacs.app /Applications/Emacs.app
