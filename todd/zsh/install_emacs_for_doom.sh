8165ddc22428f2c8660e584bf0b1363fad8728b8
Todd Ornett <toddgh@acquirus.com>
2025-03-28 12:02:03 +0900

    update doom emacs installation and configuration

--- todd/zsh/install_emacs_for_doom.sh
+++ todd/zsh/install_emacs_for_doom.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
-brew tap railwaycat/emacsmacport
-brew reinstall emacs-mac --with-dbus\
-   --with-modules\
-   --with-starter\
-   --with-imagemagick\
-   --with-no-title-bars\
-   --with-emacs-icons-project-EmacsIcon7\
-   --with-native-comp\
-   --with-mac-metal\
-   --with-xwidgets\
-  --with-tree-sitter
-ln -sfn $HOMEBREW/opt/emacs-mac/Emacs.app /Applications/Emacs.app
+brew install git ripgrep coreutils fd
+xcode-select --install
+brew tap d12frosted/emacs-plus
+brew reinstall emacs-plus@30
+ln -sfn $HOMEBREW/opt/emacs-plus/Emacs.app /Applications/Emacs.app
