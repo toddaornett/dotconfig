b7b3df1bd9ad6cffd9d52a863092cbda5f3481f2
Todd Ornett <toddgh@acquirus.com>
2025-03-31 18:42:22 +0900

    custom safe variable update

--- doom/custom.el
+++ doom/custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(yasnippet-snippets eglot)))
+ '(package-selected-packages '(yasnippet-snippets eglot))
+ '(safe-local-variable-values '((checkdoc-package-keywords-flag))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
