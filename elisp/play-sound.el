239e30c8cafedcf09c52440bbc202b786fe33daf
Todd Ornett <toddgh@acquirus.com>
2025-03-31 18:41:31 +0900

    modernized play-sound

--- elisp/play-sound.el
+++ elisp/play-sound.el
@@ -1,9 +1,10 @@
 ;;; play-sound.el --- play sound files on OSX  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2011-2013  Leo Liu
-
 ;; Author: Leo Liu <sdl.web@gmail.com>
 ;; Version: 1.0
+;; Updated: 2025-03-31 to not require cl
+;; 
 ;; Keywords: comm, tools, convenience
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -32,22 +33,27 @@
 
 ;;; Code:
 
-(eval-when-compile (require 'cl))
-
 (defun play-sound-internal (sound)
-  "Internal function for `play-sound' (which see)."
-  (or (eq (car-safe sound) 'sound)
-      (signal 'wrong-type-argument (list sound)))
+  "Internal function for `play-sound' to play SOUND."
+  (unless (and (listp sound) (eq (car sound) 'sound))
+    (signal 'wrong-type-argument (list sound)))
 
-  (destructuring-bind (&key file data volume device)
-      (cdr sound)
+  (let* ((plist (cdr sound))
+         (file (plist-get plist :file))
+         (data (plist-get plist :data))
+         (volume (plist-get plist :volume))
+         (device (plist-get plist :device))
+         (args (list "afplay")))
 
-    (and (or data device)
-         (error "DATA and DEVICE arg not supported"))
+    (when (or data device)
+      (error "DATA and DEVICE arguments are not supported"))
 
-    (apply #'start-process "afplay" nil
-           "afplay" (append (and volume (list "-v" volume))
-                            (list (expand-file-name file data-directory))))))
+    (when volume
+      (setq args (nconc args (list "-v" (format "%s" volume)))))
+
+    (setq args (nconc args (list (expand-file-name file data-directory))))
+
+    (apply #'start-process "afplay" nil args)))
 
 (provide 'play-sound)
 ;;; play-sound.el ends here
