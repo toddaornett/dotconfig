b46a296282512ec79335fcb7f1b24818de9a1b0a
Todd Ornett <toddgh@acquirus.com>
2026-03-08 08:29:20 +0900

    feat: add doom emacs template for new package

---
+++ doom/templates/__doom-package.el
@@ -0,0 +1,18 @@
+;;; `(file-name-nondirectory (or (buffer-file-name) ""))` --- $1 -*- lexical-binding: t; -*-
+
+;; Copyright (C) `(format-time-string "%Y")`
+
+;; Author: `(or user-full-name "")`
+;; Maintainer: `(or user-full-name "")`
+;; Homepage: https://github.com/toddaornett/dotconfig/tree/main/elisp/`(file-name-base (or (buffer-file-name) ""))`
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "29.1"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+
+;;; Code:
+$0
+(provide '`(file-name-base (or (buffer-file-name) ""))`)
+;;; `(file-name-nondirectory (or (buffer-file-name) ""))` ends here
