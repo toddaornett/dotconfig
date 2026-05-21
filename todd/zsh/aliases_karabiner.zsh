882f0889fa320b3558ffde80b2b76dc46b098f3b
Todd Ornett <toddgh@acquirus.com>
2026-05-22 08:42:52 +0900

    feat: use karabiner-elements cask, add aliases

---
+++ todd/zsh/aliases_karabiner.zsh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+alias karabiner-show-profile='karabiner_cli --show-current-profile-name'
+alias karabiner-list-profiles='karabiner_cli --list-profile-names'
+alias karabiner-use-laptop="karabiner_cli --select-profile 'Laptop without External Keyboard'"
+alias karabiner-use-hhkb="karabiner_cli --select-profile 'Default profile'"
