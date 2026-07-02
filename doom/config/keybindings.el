75f4c4f4b051d3220a289cd2681a54401b622428
ToddAOrnett <toddgh@acquirus.com>
2026-07-02 09:17:15 +0900

    chore: re-add org-pomodoro package

--- doom/config/keybindings.el
+++ doom/config/keybindings.el
@@ -1,6 +1,10 @@
 ;;; $DOOMDIR/config/keybindings.el --- various keybindings -*- lexical-binding: t -*-
 
 (map! :leader
-      :desc "Comment line" "-" #'comment-line)
+  :desc "Comment line" "-" #'comment-line)
 (map! :leader
-      :desc "Comment region" "=" #'comment-line)
+  :desc "Comment region" "=" #'comment-line)
+
+(map! :after org-pomodoro
+  :leader
+  :n "t t" #'org-pomodoro)
