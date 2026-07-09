2e238b233d2be84afc13b0b5c908f08e0cd7dc7e
Todd Ornett <toddgh@acquirus.com>
2026-07-10 08:54:16 +0900

    feat: improve copy path keybindings

--- doom/config/keybindings.el
+++ doom/config/keybindings.el
@@ -1,27 +1,44 @@
 ;;; $DOOMDIR/config/keybindings.el --- various keybindings -*- lexical-binding: t -*-
-
 (map! :leader
-  :desc "Comment line" "-" #'comment-line)
+      :desc "Comment line" "-" #'comment-line)
 (map! :leader
-  :desc "Comment region" "=" #'comment-line)
+      :desc "Comment region" "=" #'comment-line)
 
 (map! :after org
-  :map org-mode-map
-  :localleader
-  :n "p" #'org-pomodoro)
+      :map org-mode-map
+      :localleader
+      :n "p" #'org-pomodoro)
 
 (map! :leader
-  :desc "Reslect last region"
-  "r r" #'tao/reselect-last-region)
+      :desc "Reslect last region"
+      "r r" #'tao/reselect-last-region)
 
 (map! :leader
-  :desc "Toggle speedbar or updates"
-  :n "t s" #'tao/smart-speedbar-toggle)
+      :desc "Toggle speedbar or updates"
+      :n "t s" #'tao/smart-speedbar-toggle)
 
 (map! :leader
-  :desc "Quit ERC" "q i" #'tao/erc-quit-and-cleanup)
+      :desc "Quit ERC" "q i" #'tao/erc-quit-and-cleanup)
 
 (map! :nvi
-  "s-c" #'clipboard-kill-ring-save
-  "s-v" #'tao/paste-from-clipboard)
+      "s-c" #'clipboard-kill-ring-save
+      "s-v" #'tao/paste-from-clipboard)
 (define-key minibuffer-local-map (kbd "s-v") #'tao/paste-from-clipboard)
+
+(defun tao/copy-relative-path-dwim ()
+  "Copy relative path of file at point in Dired, or current buffer's file otherwise."
+  (interactive)
+  (if (derived-mode-p 'dired-mode)
+      (tao/dired-copy-relative-path)
+    (tao/copy-buffer-relative-path)))
+
+(defun tao/copy-full-path-dwim ()
+  "Copy full path of file at point in Dired, or current buffer's file otherwise."
+  (interactive)
+  (if (derived-mode-p 'dired-mode)
+      (tao/dired-copy-full-path)
+    (buffer-file-name)))
+
+(map! :leader
+      :desc "Copy full path"     "f p" #'tao/copy-full-path-dwim
+      :desc "Copy relative path" "f P" #'tao/copy-relative-path-dwim)
