6716c9c74307c17a8c4928270b9b3c8def0d2ec1
Todd Ornett <toddgh@acquirus.com>
2026-07-31 10:35:56 +0900

    fix: enhance the modified timestamp hook

--- doom/config/hooks.el
+++ doom/config/hooks.el
@@ -1,10 +1,15 @@
 ;;; $DOOMDIR/config/hooks.el --- various hooks -*- lexical-binding: t; -*-
+;; Created: July 30, 2026
+;; Modified: July 31, 2026
 
 (defun tao/update-modified-timestamp ()
-  (when  (derived-mode-p 'emacs-lisp-mode)
-      (save-excursion
+  (when (derived-mode-p 'emacs-lisp-mode)
+    (save-excursion
+      (save-match-data
         (goto-char (point-min))
-        (when (search-forward ";; Modified: July 30, 2026
-          (delete-region (point) (line-end-position))
-          (insert (format-time-string "%B %d, %Y"))))))
+        (let ((case-fold-search nil))
+          (when (re-search-forward "^;; Modified: " nil t)
+            (when (nth 4 (syntax-ppss (match-end 0)))
+              (delete-region (point) (line-end-position))
+              (insert (format-time-string "%B %d, %Y")))))))))
 (add-hook 'before-save-hook #'tao/update-modified-timestamp)
